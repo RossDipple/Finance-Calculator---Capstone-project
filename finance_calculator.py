24c05941b0dfce9b0f8205298d7338be70f9e073
Ross Dipple <rossdipple@live.co.uk>
2024-03-04 15:13:30 +0000

    tidied comments and code to fit rulers

--- finance_calculator.py
+++ finance_calculator.py
@@ -1,36 +1,41 @@
 import math
 
-#present two options and ask user to select.
-#use if/elif statement to proceed down correct path depending on selection.
-#make sure selection not case sensitive and stripped.
-#use while loop incase user selects incorrectly and break statement once finished to end loop.
+# Present two options and ask user to select.
+# Request input figures based on selection.
+# Perform calculation based on inputs/selection.
+# Print result back to user and loop.
+# Use while loop incase user selects incorrectly
 
 while True:
-    user_selection = input("Investment - to calculate the amount of interest on your investment.\n"
-                           "Bond - to calculate the amount you'll have to pay on a home loan.\n"
-                           "Please select Investment or Bond to proceed: ")
+    user_selection = input(
+        "Investment - to calculate the amount of interest on your investment.\n"
+        "Bond - to calculate the amount you'll have to pay on a home loan.\n"
+        "Please select Investment or Bond to proceed: ")
 
     user_selection = user_selection.strip()
     user_selection = user_selection.lower()
 
-    #if investment selected, request further inputs for set of calculations.
-    #wrap the code in try / except statement to allow for input errors and loop.
-    #cast inputs to relevant data types, and strip/lower case the simple or compound input.
+    # If investment selected, request further inputs for calculations.
+    # Wrap in try / except statement to allow for input errors and loop.
 
     if user_selection == "investment":
         print("Thank you, Investment selected.")
         try:
-            deposit = int(input("Please enter the amount of money you are depositing: £"))
-            interest_rate = float(input("Please enter the interest rate (as a percentage): %"))
-            num_years = int(input("Please enter the number of years you are investing: "))
-            interest_type = input("Please enter whether you want simple or compound interest?: ").lower().strip()
+            deposit = int(input(
+                "Please enter the amount of money you are depositing: £"))
+            interest_rate = float(input(
+                "Please enter the interest rate (as a percentage): %"))
+            num_years = int(input(
+                "Please enter the number of years you are investing: "))
+            interest_type = input(
+                "Please enter whether you want simple or compound interest?: ")
             interest_type = interest_type.strip()
             interest_type = interest_type.lower()
 
-            #set the values for the calculation variables before the equation/calculation.
-            #use nested if statement to select which calculation to use based on user input.
-            #round the final amount to 2 decimal places to present neatly.
-            #print final amount in f string and break the loop.
+            # Set values for the calculation variables.
+            # Select which calculation to use based on user input.
+            # Round the amount to 2 decimal places to present neatly.
+            # Print final amount in f string and break the loop.
 
             r = (interest_rate / 100)
             P = deposit
@@ -38,32 +43,37 @@ while True:
 
             if interest_type == "simple":
                 amount = P * (1 + r * t)
-                print(f"The amount you will earn on your investment is £{amount}.")
+                print("The amount you will earn on your investment "
+                      f"is £{amount}.")
                 break
             elif interest_type == "compound":
                 amount = P * math.pow((1 + r), t)
                 amount = round(amount, 2)
-                print(f"The amount you will earn on your investment is £{amount}.")
+                print("The amount you will earn on your investment "
+                      f"is £{amount}.")
                 break
             else:
                 print("Invalid input selection.")
         except ValueError:
             print("Invalid input. Please check you entered correctly.")
 
-    #if bond selected, request further inputs for calculation.
-    #wrap the code in try / except statement to allow for input errors and loop.
-    #cast inputs to relevant data types.
+    # If bond selected, request further inputs for calculation.
+    # Wrap in try / except statement to allow for input errors and loop.
 
     elif user_selection == "bond":
         print("Thank you, Bond selected.")
         try:
-            house_value = int(input("Please enter how much your house is worth: £"))
-            interest_rate = float(input("Please enter the interest rate (as a percentage): %"))
-            num_months = int(input("Please enter the number of months you plan to take to repay the bond: "))
+            house_value = int(input(
+                "Please enter how much your house is worth: £"))
+            interest_rate = float(input(
+                "Please enter the interest rate (as a percentage): %"))
+            num_months = int(input(
+                "Please enter the number of months you plan to take to repay "
+                "the bond: "))
 
-            #set the values for the calculation variables before the equation/calculation.
-            #round repayment to 2 decimal places to present neatly.
-            #print repayment sum in f string and break the loop.
+            # Set values for the calculation variables.
+            # Round repayment to 2 decimal places to present neatly.
+            # Print repayment sum in f string and break the loop.
 
             P = house_value
             i = (interest_rate / 100) / 12
@@ -71,7 +81,8 @@ while True:
 
             repayment = (i * P) / (1 - (1 + i) ** (-n))
             repayment = round(repayment, 2)
-            print(f"The amount you will have to pay each month is £{repayment}.")
+            print("The amount you will have to pay each month is "
+                  f"£{repayment}.")
             break
         except ValueError:
             print("Invalid input. Please check you entered correctly.")
